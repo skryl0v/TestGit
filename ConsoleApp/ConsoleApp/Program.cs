c6b4d498a5767dc4c8b88f16accb2e45d2506ac5
skryl0v <skrylov67@gmail.com>
2016-10-25 09:53:06 +0300

    Update Program.cs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp
         {
             Console.WriteLine("HelloWorld");
             Console.WriteLine("HelloWorld");
+            Console.WriteLine("HelloWorld");
             Console.ReadLine();
         }
     }
